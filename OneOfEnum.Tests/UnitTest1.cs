d8fee7baa76f83af9c7640cb36aef26d83dfdb5e
de18702 <stahl.d@loh-services.de>
2022-03-27 17:36:23 +0200

    Add project files.

---
+++ OneOfEnum.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace OneOfEnum.Tests
+{
+    public enum TestEnum
+    {
+        Yes,
+        No,
+        Maybe
+    }
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
